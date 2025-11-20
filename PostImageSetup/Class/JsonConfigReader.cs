ead246b6f91498e0ce76fdbcb3e7192e274595c9
tbodenner <204929239+tbodenner@users.noreply.github.com>
2025-11-20 09:35:12 -0700

    Added new windows for each config file.

--- PostImageSetup/Class/JsonConfigReader.cs
+++ PostImageSetup/Class/JsonConfigReader.cs
@@ -1,36 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Text.Json;
-using System.IO;
-using PostImageSetup.Class.SiteConfigs.Site;
+﻿using PostImageSetup.Class.SiteConfigs.Baseline;
 using PostImageSetup.Class.SiteConfigs.Installs;
-using PostImageSetup.Class.SiteConfigs.Baseline;
+using PostImageSetup.Class.SiteConfigs.Site;
+using System.IO;
+using System.Text.Json;
 
 namespace PostImageSetup.Class
 {
-  internal class JsonConfigReader
+  internal static class JsonConfigReader
   {
     private static readonly string TESTFilePath = "C:\\Users\\OITPREBODENT\\OneDrive - Department of Veterans Affairs\\Desktop\\GitHub\\Install-PostImageSetup";
-    public void ReadConfig()
+
+    private static string? GetJsonText(string jsonFilePath)
     {
-      string configPath = Path.Join(TESTFilePath, "config.json");
-      string jsonFile = File.ReadAllText(configPath);
-      SiteConfig? testConfig = JsonSerializer.Deserialize<SiteConfig>(jsonFile);
+      if (File.Exists(jsonFilePath))
+      {
+        return File.ReadAllText(jsonFilePath);
+      }
+      else
+      {
+        return null;
+      }
     }
-    public void ReadInstalls()
+
+    public static SiteConfig? ReadConfig()
     {
-      string configPath = Path.Join(TESTFilePath, "installs.json");
-      string jsonFile = File.ReadAllText(configPath);
-      InstallConfig? testConfig = new(JsonSerializer.Deserialize<InstallItem[]>(jsonFile));
+      string? jsonText = GetJsonText("config.json");
+      if (jsonText == null)
+      {
+        return null;
+      }
+      else
+      {
+        return JsonSerializer.Deserialize<SiteConfig>(jsonText);
+      }
     }
-    public void ReadBaseline()
+
+    public static InstallConfig? ReadInstalls()
     {
-      string configPath = Path.Join(TESTFilePath, "baseline.json");
-      string jsonFile = File.ReadAllText(configPath);
-      BaselineConfig? testConfig = new(JsonSerializer.Deserialize<Dictionary<string,string[]>>(jsonFile));
+      string? jsonText = GetJsonText("installs.json");
+      if (jsonText == null)
+      {
+        return null;
+      }
+      else
+      {
+        return new(JsonSerializer.Deserialize<InstallItem[]>(jsonText));
+      }
+    }
+
+    public static BaselineConfig? ReadBaseline()
+    {
+      string? jsonText = GetJsonText("baseline.json");
+      if (jsonText == null)
+      {
+        return null;
+      }
+      else
+      {
+        return new(JsonSerializer.Deserialize<Dictionary<string, string[]>>(jsonText));
+      }
     }
   }
 }
--- PostImageSetup/MainWindow.xaml.cs
+++ PostImageSetup/MainWindow.xaml.cs
@@ -1,15 +1,9 @@
 ﻿using PostImageSetup.Class;
 using PostImageSetup.Class.AppConfig;
-using System.Text;
+using PostImageSetup.Class.SiteConfigs.Baseline;
+using PostImageSetup.Class.SiteConfigs.Installs;
+using PostImageSetup.Class.SiteConfigs.Site;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace PostImageSetup
 {
@@ -18,14 +12,45 @@ namespace PostImageSetup
   /// </summary>
   public partial class MainWindow : Window
   {
+    private readonly SiteConfig? _siteConfig;
+    private readonly InstallConfig? _installConfig;
+    private readonly BaselineConfig? _baselineConfig;
+
     public MainWindow()
     {
       InitializeComponent();
       AppConfigController acc = new();
-      JsonConfigReader jcf = new();
-      jcf.ReadConfig();
-      jcf.ReadInstalls();
-      jcf.ReadBaseline();
+      _siteConfig = JsonConfigReader.ReadConfig();
+      _installConfig = JsonConfigReader.ReadInstalls();
+      _baselineConfig = JsonConfigReader.ReadBaseline();
+    }
+
+    private void InstallConfigButton_Click(object sender, RoutedEventArgs e)
+    {
+      Window window = new InstallConfigWindow
+      {
+        Owner = this,
+        WindowStyle = WindowStyle.ToolWindow
+      };
+      window.ShowDialog();
+    }
+    private void BaselineConfigButton_Click(object sender, RoutedEventArgs e)
+    {
+      Window window = new BaselineConfigWindow
+      {
+        Owner = this,
+        WindowStyle = WindowStyle.ToolWindow
+      };
+      window.ShowDialog();
+    }
+    private void SiteConfigButton_Click(object sender, RoutedEventArgs e)
+    {
+      Window window = new SiteConfigWindow
+      {
+        Owner = this,
+        WindowStyle = WindowStyle.ToolWindow
+      };
+      window.ShowDialog();
     }
   }
 }
